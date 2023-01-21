1e4626890f2468ae7fdf625c208d79b35cc483df
AndikaDwiRachmanP <93780334+AndikaDwiRachmanP@users.noreply.github.com>
2023-01-22 04:06:59 +0700

    tampilan
    tampilan buanyak

--- resources/views/clientside/detail.blade.php
+++ resources/views/clientside/detail.blade.php
@@ -5,36 +5,66 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/style.css">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
 
     <title>Pena</title>
 
 </head>
 <header>
-    <div>
-        <nav class="static top-0">
-            <div class="flex flex-row bg-[#5abbc0] w-auto h-20" >
+<div>
+        <nav>
+            <div class="flex flex-row bg-[#5abbc0] w-auto h-20 p-6" >
                 <div class="ml-3 text-[30px] font-medium m-auto text-black">PE<span class=" text-blue-600">NA</span></div>
-                <div class="ml-auto"><button class="rounded-sm bg-blue-600 w-20 h-auto hover:bg-white text-[20px] font-medium m-7 text-white hover:text-black">Home</button></div>
-                <div class="-ml-12"><button class="rounded-sm w-20 h-auto hover:bg-white text-[20px] font-medium m-7">About</button></div>
-                <button class="space-y-2 -ml-6 m-7 hover:bg-white" >
-                    <div class="w-8 h-0.5 bg-gray-600"></div>
-                    <div class="w-8 h-0.5 bg-gray-600"></div>
-                    <div class="w-8 h-0.5 bg-gray-600"></div>
-                </button>
+                <div class="ml-auto"><a class="p-3 rounded-sm bg-transparent w-20 h-auto hover:bg-white text-[20px] font-medium m-7 text-black hover:text-black " href='/'>Home</a></div>
+                <div class="-ml-12"><a class=" p-3 rounded-sm w-20 h-auto hover:bg-white text-[20px] font-medium m-7" href="/about" >About</a></div>
+              <div class="" x-data="{showMenu : false}">
+                    <button @click.prevent="showMenu = !showMenu " class="px-2 py-4 flex justify-between bg-blue-500 text-white -mt-3">
+                        <svg x-show="!showMenu" class="w-6 h-6 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                        <svg x-show="showMenu" class="w-6 h-6 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
+                         Menu
+                    </button>
+                 <div x-show="showMenu"> 
+                    <nav class="flex flex-col absolute z-50">
+                            <a href="serverside/master" class="px-2 py-4 text-blue-600 bg-white flex justify-between w-full border-b border-blue-900 hover:text-blue-100 hover:bg-blue-600">
+                                Login
+                                <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
+                            </a>
+                            <a href="#" class="px-2 py-4 text-blue-600 bg-white flex justify-between w-full border-b border-blue-900 hover:text-blue-100 hover:bg-blue-600">
+                            Genre
+                            <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
+                            </a>
+                            <a href="#" class="px-2 py-4 text-blue-600 bg-white flex justify-between w-full border-b border-blue-900 hover:text-blue-100 hover:bg-blue-600">
+                            Daftar Buku
+                            <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
+                            </a>
+                            <a href="#" class="px-2 py-4 text-blue-600 bg-white flex justify-between w-full border-b border-blue-900 hover:text-blue-100 hover:bg-blue-600">
+                            
+                            <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
+                            </a>
+                            <a href="#" class="px-2 py-4 text-blue-600 bg-white flex justify-between w-full border-b border-blue-900 hover:text-blue-100 hover:bg-blue-600">
+                            bsx
+                            <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
+                            </a>
+                    </nav>
+                 </div>
+              </div>
             </div>
         </nav>
     </div>
 </header>
 
 <body>
-    <div class="w-[60rem] h-[30rem] bg-slate-200 mx-auto mt-5 mb-5 rounded-md">
+    <div class="w-[60rem] h-auto bg-slate-200 mx-auto mt-5 mb-5 rounded-md p-4">
         <div class="mt-2 ml-2">
             <p class="text-center font-bold text-xl">{{$data->judul_buku}}</p>
             <p class=" w-20 h-6">{{$data->nama_kategori}}</p>
             <p class="text-center">
-            <img src="{{url('./image/book')}}\{{$data->gambar}}" class= "w-3/4" alt="" />
+            <img src="{{url('./image/book')}}\{{$data->gambar}}" class= "w-48 mx-auto" alt="" />
             </p>
-            <p>{{$data->sinopsis}}</p>
+            <p class="text-justify">{{$data->sinopsis}}</p>
         </div>
 
     </div>
